2d1e2e59af1e2a303db968321da390246148a177
Emmanuel Daniel <ecomje@gmail.com>
2019-10-15 09:54:40 +0100

    create card component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CardList from "./component/card-list/card-list.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      title: 'Levelling Up on ReactJs Basic (Again 2.1)',
+      users: [],
+      showCards: false
+    }
+  }
+  componentDidMount() {
+    fetch('https://randomuser.me/api/?results=30')
+        .then(response => response.json())
+        .then(users => this.setState({ users: users.results }))
+  }
+
+  render() {
+    setTimeout(()=> {
+      this.setState({ ...this.state, showCards: true })
+    }, 4000);
+    return <>
+      <div className="App">
+        <header className="App-header">
+          <h1>{this.state.title}</h1>
+          {!this.state.showCards && <p>please, wait loading contents </p>}
+         {this.state.showCards && <CardList users={this.state.users}/>}
+        </header>
+      </div>
+    </>;
+  }
 }
 
 export default App;
